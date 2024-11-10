379a227b1d88d2efbb1433c0f4cc3f7cec26b0dd
NDHUCSIE-CHEN <411321263@gms.ndhu.edu.tw>
2024-11-10 23:20:43 +0800

    Update category.php

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
  mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
  if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
      die('Failed to connect to MySQL: '.mysqli_connect_error());
- }
+ }else echo"好欸終於我要哭了";
 ?>
 
 <!DOCTYPE html>
