d236bc1ba35f796283ad28771b599af371aed8d9
NDHUCSIE-CHEN <411321263@gms.ndhu.edu.tw>
2024-11-11 10:52:09 +0800

    Update user.php

--- user.php
+++ user.php
@@ -305,7 +305,8 @@
         }
     </script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </section>
 
 
